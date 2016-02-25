a873577b00d1913bb34a7230a1e59345b062728d
auntie <auntie.joye@gmail.com>
2016-02-25 18:32:06 +0300

    Removed logging to console.

--- insertionSort.js
+++ insertionSort.js
@@ -9,11 +9,9 @@
             for (var j = i - 1; j >= 0; j--) {
                 if (array[j] > item) {
                     array[j + 1] = array[j];
-                    print(array);
                     continue;
                 } else if (j + 1 != i) {
                     array[j + 1] = item;
-                    print(array);
                 }
 
                 break;
@@ -21,7 +19,6 @@
 
             if (j < 0 && (j + 1 != i)) {
                 array[j + 1] = item;
-                print(array);
             }
         }
     };
