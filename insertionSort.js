c9eb967d4c180bca084c1e938c42dc05cd31614b
auntie <auntie.joye@gmail.com>
2016-02-25 18:22:15 +0300

    Added simple sort algorithm - insertion sort.

---
+++ insertionSort.js
@@ -0,0 +1,32 @@
+(function() {
+    "use strict";
+
+    function Insertion() {}
+
+    Insertion.sort = function(array) {
+        for (var i = 1; i < array.length; i++) {
+            var item = array[i];
+            for (var j = i - 1; j >= 0; j--) {
+                if (array[j] > item) {
+                    array[j + 1] = array[j];
+                    print(array);
+                    continue;
+                } else if (j + 1 != i) {
+                    array[j + 1] = item;
+                    print(array);
+                }
+
+                break;
+            }
+
+            if (j < 0 && (j + 1 != i)) {
+                array[j + 1] = item;
+                print(array);
+            }
+        }
+    };
+
+    Insertion.print = function(array) {
+        console.log(array.join(" "));
+    };
+}());
